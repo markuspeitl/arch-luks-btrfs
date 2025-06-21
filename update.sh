2200b47b47785329552c06054ac56c44e99c9ade
Markus Peitl <office@markuspeitl.com>
2025-06-21 15:08:38 +0200

    Updated loader conf and fstab

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
-history > "logs/$USER_history.log"
-cp -ap /etc/fstab etc/fstab
-cp -ap /etc/mkinitcpio* etc/
+#!/usr/bin/env bash
+
+# history > "logs/$(whoami)_history.log"
 
 cp -rap /boot/loader/* boot/loader
 cp -rap /efi/loader/* efi/loader
+
+cp -ap /etc/fstab etc/fstab
+cp -ap /etc/mkinitcpio* etc/
