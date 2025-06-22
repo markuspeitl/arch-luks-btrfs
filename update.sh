488eaffd950d330b8176ff91cae65b7d9f5d6486
Markus Peitl <office@markuspeitl.com>
2025-06-22 14:31:37 +0200

    Fixed initcpio presets for rebuilding with nvidia modules

--- update.sh
+++ update.sh
@@ -11,6 +11,41 @@ cp -rap /boot/loader/* "$SCRIPT_D/boot/loader"
 cp -rap /efi/loader/* "$SCRIPT_D/efi/loader"
 
 cp -ap /etc/fstab "$SCRIPT_D/etc/fstab"
-cp -ap /etc/mkinitcpio* "$SCRIPT_D/etc/"
+cp -apr /etc/mkinitcpio* "$SCRIPT_D/etc/"
 
+
+
+PWD=$(pwd)
+
+function mktouch(){
+	mkdir -p $(dirname "$SCRIPT_D/$1")
+	touch "$SCRIPT_D/$1"
+}
+
+find /boot -iname "*.img" | xargs mktouch
+find /boot -iname "*.efi" | xargs mktouch
+
+
+#PWD=$(pwd)
+#cd /boot
+#BOOT_IMGS=$(find . -iname "*.img")
+#for FILE in $BOOT_IMGS
+#do
+#	echo "$SCRIPT_D/$(dirname $FILE)"
+#	#mkdir -p "$SCRIPT_D/$(dirname $FILE)"
+#done
+#cd "$PWD"
+
+
+mkdir -p $SCRIPT_D/etc/systemd/network
+cp /etc/systemd/network/* "$SCRIPT_D/etc/systemd/network"
+#sudo systemctl restart systemd-networkd.service
+#sudo systemctl restart systemd-daemon
+
+
+PWD=$(pwd)
+cd "$SCRIPT_D"
 git restore efi/loader/random-seed
+cd "$PWD"
+
+
